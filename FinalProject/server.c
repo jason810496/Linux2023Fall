efe5b4b97f008aa8bec0d827da67dbab2030e22b
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-12-15 20:28:12 +0800

    add comment

--- FinalProject/server.c
+++ FinalProject/server.c
@@ -35,6 +35,8 @@ int main(int argc, char const* argv[])
     // if (setsockopt(server_fd, SOL_SOCKET,
     //                SO_REUSEADDR | SO_REUSEPORT, &opt,
     //                sizeof(opt))) {
+        
+    // On Mac OS X, SO_REUSEPORT is not a valid option.
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
 
         perror("setsockopt");
