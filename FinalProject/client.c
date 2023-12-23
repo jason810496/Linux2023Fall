479d2c3ef330517b42b3cbf2369473bf6f4f7f2f
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-12-23 12:49:08 +0800

    add comments & format

--- FinalProject/client.c
+++ FinalProject/client.c
@@ -8,89 +8,112 @@
 #define PORT 8080
 #include "color.h"
 
-const char START_CONNECT[] = "START_CONNECT";
-const char START[] = "START";
-int DEMO = 0;
+static int DEMO = 0;
 
-void ServerConnectHint(){
-    if( DEMO ){
+/* pre-defined messages */
+// client will send START_CONNECT when first connected
+const char START_CONNECT[] = "START_CONNECT";
+// server will send START when first connected
+const char START[] = "START";
+
+void ServerConnectHint()
+{
+    if (DEMO)
+    {
         printf(GREEN "Connect to server!\n" RESET);
     }
 }
 
-void ServerReceiveHint(char *buffer){
-    if( DEMO ){
-        printf("%sServer:%s%s\n",BLUE,RESET,buffer);
+void ServerReceiveHint(char *buffer)
+{
+    if (DEMO)
+    {
+        printf("%sServer:%s%s\n", BLUE, RESET, buffer);
         return;
     }
+
+    // server will send START when first connected
     char *match = strstr(buffer, START);
-    if (match != NULL) {
+    if (match != NULL)
+    {
         return;
     }
     printf("%s\n", buffer);
 }
 
-void UserInputHint(){
-    if( DEMO ){
-        printf( GREEN "Enter a message to send to the server: " RESET);
+void UserInputHint()
+{
+    if (DEMO)
+    {
+        printf(GREEN "Enter a message to send to the server: " RESET);
     }
 }
 
-
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
-    if( argc == 2 ){
-        if( strcmp(argv[1],"demo") == 0 ){
+    // if `demo` is passed in as an argument
+    // client will print out the helper message
+    if (argc == 2)
+    {
+        if (strcmp(argv[1], "demo") == 0)
+        {
             DEMO = 1;
         }
     }
 
-	int status, val_read, client_fd;
-	struct sockaddr_in serv_addr;
-	char* hello = "Hello from client";
-	char buffer[1024] = { 0 };
-	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("\n Socket creation error \n");
-		return -1;
-	}
+    // socket connection from geeksforgeeks
+    int status, val_read, client_fd;
+    struct sockaddr_in serv_addr;
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+    char buffer[1024] = {0};
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
-	// Convert IPv4 and IPv6 addresses from text to binary
-	// form
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-		<= 0) {
-		printf(
-			"\nInvalid address/ Address not supported \n");
-		return -1;
-	}
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-	if ((status
-		= connect(client_fd, (struct sockaddr*)&serv_addr,
-				sizeof(serv_addr)))
-		< 0) {
-		printf("\nConnection Failed \n");
-		return -1;
-	}
+    // Convert IPv4 and IPv6 addresses from text to binary
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
-	send(client_fd, START_CONNECT, strlen(START_CONNECT), 0);
-	
+    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr,
+                          sizeof(serv_addr))) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // client will send START_CONNECT when first connected
+    send(client_fd, START_CONNECT, strlen(START_CONNECT), 0);
     ServerConnectHint();
 
-    while ((val_read = read(client_fd, buffer, 1024 - 1)) > 0) {
+    // main logic of the client
+    while ((val_read = read(client_fd, buffer, 1024 - 1)) > 0)
+    {
         buffer[val_read] = '\0';
+        // helper function to print out the server's message
+        // if `DEMO` is set to 1, then it will print out the helper message
         ServerReceiveHint(buffer);
 
+        // helper function to print out the user input hint ( if `DEMO` is set to 1)
         UserInputHint();
         char input[1024];
         // read until newline
         fgets(input, 1024, stdin);
         send(client_fd, input, strlen(input), 0);
+
+        // clear the buffer
         memset(buffer, 0, 1024);
     }
 
-	// closing the connected socket
-	close(client_fd);
-	return 0;
+    // closing the connected socket
+    close(client_fd);
+    return 0;
 }
--- FinalProject/server.c
+++ FinalProject/server.c
@@ -7,12 +7,32 @@
 #define PORT 8080
 #include "color.h"
 
-int DEMO = 0;
+static int DEMO = 0;
 
-int main(int argc, char const* argv[])
+/* pre-defined messages */
+// client will send START_CONNECT when first connected
+const char START_CONNECT[] = "START_CONNECT";
+// server will send START when first connected
+const char START[] = "START";
+
+/* operations */
+const char ADD[] = "add";
+const char ABS[] = "abs";
+const char MUL[] = "mul";
+const char KILL[] = "kill";
+const char HELLO[] = "Hello";
+const char SPACE[] = " ";
+
+// if `DEMO` is set to 1
+// server will print out the helper message instead of `Hello`
+const char INVALID[] = "Invalid command!";
+
+int main(int argc, char const *argv[])
 {
-    if( argc == 2 ){
-        if( strcmp(argv[1],"demo") == 0 ){
+    if (argc == 2)
+    {
+        if (strcmp(argv[1], "demo") == 0)
+        {
             DEMO = 1;
         }
     }
@@ -22,22 +42,23 @@ int main(int argc, char const* argv[])
     struct sockaddr_in address;
     int opt = 1;
     socklen_t addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
- 
+    char buffer[1024] = {0};
+
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
- 
+
     // Forcefully attaching socket to the port 8080
     // if (setsockopt(server_fd, SOL_SOCKET,
     //                SO_REUSEADDR | SO_REUSEPORT, &opt,
     //                sizeof(opt))) {
-        
-    // On Mac OS X, SO_REUSEPORT is not a valid option.
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
+
+    /* On Mac OS X, SO_REUSEPORT is not a valid option. */
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+    {
 
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -45,74 +66,75 @@ int main(int argc, char const* argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
- 
+
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  &addrlen))
-        < 0) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             &addrlen)) < 0)
+    {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    const char START_CONNECT[] = "START_CONNECT";
-    const char START[] = "START";
-    const char ADD[] = "add";
-    const char ABS[] = "abs";
-    const char MUL[] = "mul";
-    const char KILL[] = "kill";
-    const char HELLO[] = "Hello";
-    const char INVALID[] = "Invalid command!";
-
-    while ((val_read = read(new_socket, buffer, 1024 - 1)) > 0) {
-        printf("%sClient:%s%s",BLUE,RESET,buffer);
+    while ((val_read = read(new_socket, buffer, 1024 - 1)) > 0)
+    {
+        printf("%sClient:%s%s", BLUE, RESET, buffer);
 
         char output[1024] = {};
         int invalid = 1;
 
-        if (strcmp(buffer, START_CONNECT) == 0) {
+        // server will send START_CONNECT when first connected
+        if (strcmp(buffer, START_CONNECT) == 0)
+        {
             invalid = 0;
             printf(GREEN "\nConnect to client!\n" RESET);
             sprintf(output, "%s", START);
             send(new_socket, output, strlen(output), 0);
-        } 
+        }
 
-
-        
-
+        // match `add` operation
         char *match_ptr = strstr(buffer, ADD);
-        if (match_ptr != NULL) {
+        if (match_ptr != NULL)
+        {
             invalid = 0;
-            char *token = strtok(buffer, " ");
+            // parse the string by space
+            char *token = strtok(buffer, SPACE);
             int sum = 0;
-            while (token != NULL) {
-                token = strtok(NULL, " ");
-                if (token != NULL) {
+            while (token != NULL)
+            {
+                token = strtok(NULL, SPACE);
+                if (token != NULL)
+                {
                     sum += atoi(token);
                 }
             }
+            // stdout to string
             sprintf(output, "%d", sum);
             send(new_socket, output, strlen(output), 0);
         }
 
+        // match `abs` operation
         match_ptr = strstr(buffer, ABS);
-        if (match_ptr != NULL) {
+        if (match_ptr != NULL)
+        {
             invalid = 0;
-            char *token = strtok(buffer, " ");
+            char *token = strtok(buffer, SPACE);
             int num = 0;
-            while (token != NULL) {
-                token = strtok(NULL, " ");
-                if (token != NULL) {
+            while (token != NULL)
+            {
+                token = strtok(NULL, SPACE);
+                if (token != NULL)
+                {
                     num = atoi(token);
                 }
             }
@@ -120,14 +142,18 @@ int main(int argc, char const* argv[])
             send(new_socket, output, strlen(output), 0);
         }
 
+        // match `mul` operation
         match_ptr = strstr(buffer, MUL);
-        if (match_ptr != NULL) {
+        if (match_ptr != NULL)
+        {
             invalid = 0;
-            char *token = strtok(buffer, " ");
+            char *token = strtok(buffer, SPACE);
             int mul = 1;
-            while (token != NULL) {
-                token = strtok(NULL, " ");
-                if (token != NULL) {
+            while (token != NULL)
+            {
+                token = strtok(NULL, SPACE);
+                if (token != NULL)
+                {
                     mul *= atoi(token);
                 }
             }
@@ -135,24 +161,32 @@ int main(int argc, char const* argv[])
             send(new_socket, output, strlen(output), 0);
         }
 
+        // match `kill` operation
         match_ptr = strstr(buffer, KILL);
-        if (match_ptr != NULL) {
+        if (match_ptr != NULL)
+        {
             printf(RED "\nServer is killed!\n" RESET);
-            break;  
+            break;
         }
 
-        if ( invalid ) {
-            if( DEMO ){
+        // if no match
+        if (invalid)
+        {
+            // print out the helper message
+            // if `DEMO` is set to 1
+            if (DEMO)
+            {
                 send(new_socket, INVALID, strlen(INVALID), 0);
             }
-            else{
+            else
+            {
                 send(new_socket, HELLO, strlen(HELLO), 0);
             }
         }
 
         memset(buffer, 0, sizeof(buffer));
     }
- 
+
     // closing the connected socket
     close(new_socket);
     // closing the listening socket
